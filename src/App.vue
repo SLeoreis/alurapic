5184df7e6e64c7892759d8c77bdb511d1912b18a
Leonardo Reis <leonardo.reis@atsinformatica.com.br>
2022-03-28 14:43:44 -0300

    App vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>{{titulo}}</h1>
+  <img src="{{ foto.url }}" alt= "{{ foto.titulo }}">
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            titulo: 'AluraPic',
+            foto: {
+                url: 'https://love.doghero.com.br/wp-content/uploads/2018/03/shutterstock_154829555-1024x680.webp',
+                titulo: 'Cachorro'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
